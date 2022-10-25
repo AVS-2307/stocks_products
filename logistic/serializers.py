24f4a09dc21a32189a058b45eb5e59c787c0acba
AVS-2307 <andrey230781.an@gmail.com>
2022-10-25 17:51:08 +0300

    change psw serializers.py trial ver

--- logistic/serializers.py
+++ logistic/serializers.py
@@ -23,43 +23,45 @@ class StockSerializer(serializers.ModelSerializer):
         model = Stock
         fields = ['id', 'address', 'positions']
 
+    # def update_or_create(self, instance, validated_data):
+    #     # достаем связанные данные для других таблиц
+    #     positions = validated_data.pop('positions')
+    #     stock = super().update_or_create(instance, validated_data)
+    #     StockProduct.objects.update_or_create(stock=['stock_id'], product=['product_id'], defaults={'price': positions.price, 'quantity': positions.quantity})
+    #     return stock
+
     def create(self, validated_data):
         positions = validated_data.pop('positions')
         stock = Stock.objects.create(**validated_data)
         for position in positions:
             StockProduct.objects.create(stock=stock, **position)
+        # print(positions)
         return stock
 
     # настройте сериализатор для склада
 
-    def create(self, validated_data):
-        # достаем связанные данные для других таблиц
-        positions = validated_data.pop('positions')
-        # создаем склад по его параметрам
-        stock = super().create(validated_data)
-        # здесь вам надо заполнить связанные таблицы
-        for position in positions:
-            StockProduct.objects.create(stock=stock, **position)
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
-        return stock
+    # def update(self, instance, validated_data):
+    #     #     # достаем связанные данные для других таблиц
+    #     positions = validated_data.pop('positions')
+    #     #     # обновляем склад по его параметрам
+    #     #     stock = super().update(instance, validated_data)
+    #     stock = instance.positions
+    #     # position_info
+    #     instance.stock_id = validated_data.get('stock_id', instance.stock_id)
+    #     instance.product_id = validated_data.get('product_id', instance.product_id)
+    #     instance.quantity = validated_data.get('quantity', instance.quantity)
+    #     instance.price = validated_data.get('price', instance.price)
+    #     instance.save()
+    #
+    #     return stock
 
     def update(self, instance, validated_data):
-        # достаем связанные данные для других таблиц
+        #     # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
-        # обновляем склад по его параметрам
+        #     # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
-
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+        #     # в нашем случае: таблицу StockProduct
+        #     # с помощью списка positions
         for position in positions:
             StockProduct.objects.update(**position)
-
         return stock
-
-
-
-
