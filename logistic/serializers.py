4a7c1c80a9c539913384aa488fe2b801f6c45d94
AVS-2307 <andrey230781.an@gmail.com>
2022-10-26 14:26:25 +0300

    change psw in settings.py method create_or_update was implemented requirements.txt were updated

--- logistic/serializers.py
+++ logistic/serializers.py
@@ -17,52 +17,35 @@ class ProductPositionSerializer(serializers.ModelSerializer):
 
 
 class StockSerializer(serializers.ModelSerializer):
-    positions = ProductPositionSerializer(many=True, partial=True)
+    positions = ProductPositionSerializer(many=True)
 
     class Meta:
         model = Stock
         fields = ['id', 'address', 'positions']
-
-    # def update_or_create(self, instance, validated_data):
-    #     # достаем связанные данные для других таблиц
-    #     positions = validated_data.pop('positions')
-    #     stock = super().update_or_create(instance, validated_data)
-    #     StockProduct.objects.update_or_create(stock=['stock_id'], product=['product_id'],
-    #     defaults={'price': positions.price, 'quantity': positions.quantity})
-    #     return stock
 
     def create(self, validated_data):
         positions = validated_data.pop('positions')
         stock = super().create(validated_data)
         for position in positions:
             StockProduct.objects.create(stock=stock, **position)
-        # print(positions)
+
         return stock
 
     # настройте сериализатор для склада
 
-    # def update(self, instance, validated_data):
-    #     #     # достаем связанные данные для других таблиц
-    #     positions = validated_data.pop('positions')
-    #     #     # обновляем склад по его параметрам
-    #     #     stock = super().update(instance, validated_data)
-    #     stock = instance.positions
-    #     # position_info
-    #     instance.stock_id = validated_data.get('stock_id', instance.stock_id)
-    #     instance.product_id = validated_data.get('product_id', instance.product_id)
-    #     instance.quantity = validated_data.get('quantity', instance.quantity)
-    #     instance.price = validated_data.get('price', instance.price)
-    #     instance.save()
-    #
-    #     return stock
-
     def update(self, instance, validated_data):
         #     # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-        #     # обновляем склад по его параметрам
+        # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
-        #     # в нашем случае: таблицу StockProduct
-        #     # с помощью списка positions
+        # здесь вам надо обновить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
         for position in positions:
-            StockProduct.objects.update(**position)
+            StockProduct.objects.update_or_create(
+                stock=stock,
+                product=position['product'],
+                defaults={'quantity': position['quantity'],
+                          'price': position['price']}
+            )
         return stock
--- logistic/views.py
+++ logistic/views.py
@@ -1,10 +1,8 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import UpdateModelMixin
 from rest_framework.viewsets import ModelViewSet
 
-from logistic.models import Product, Stock, StockProduct
+from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
 
@@ -22,12 +20,3 @@ class StockViewSet(ModelViewSet):
     ordering_fields = ['id', 'address', 'products']
     filterset_fields = ['address', 'products']
     search_fields = ['products__description']  # поиск продукта на складах по названию
-
-    # def get_serializer(self, instance=None, data=None, many=False, partial=False):
-    #     """If request is not PUT, allow partial updates."""
-    #     if self.request.method == 'PUT':
-    #         return StockSerializer(instance=instance, data=data, many=many, partial=True)
-    #     else:
-    #         return StockSerializer(instance=instance, data=data, many=many, partial=partial)
-
-
